4498b025c0914c0ff9dd71a73ad32163152b7e78
Archana Shetty <134158906+Archana-SS@users.noreply.github.com>
2025-06-22 23:25:17 +0530

    13. RearrangeArrayElementsBySign.java

--- 13. RearrangeArrayElementsBySign.java
+++ 13. RearrangeArrayElementsBySign.java
@@ -18,3 +18,35 @@ class Solution {
         return ans;
     }
 }
+
+//If number of positives and negatives are unequal in an array
+
+class Solution {
+    void rearrange(ArrayList<Integer> arr) {
+        int N=arr.size();
+        ArrayList<Integer> pos=new ArrayList<>();
+        ArrayList<Integer> neg=new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            if(arr.get(i)>=0)
+                pos.add(arr.get(i));
+            else
+                neg.add(arr.get(i));
+        }
+
+        int p=0,n=0,i=0;
+
+        while(p<pos.size() && n<neg.size()){
+            arr.set(i++,pos.get(p++));
+            arr.set(i++,neg.get(n++));
+        }
+
+        while(p<pos.size()){
+            arr.set(i++,pos.get(p++));
+        }
+
+        while(n<neg.size()){
+            arr.set(i++,neg.get(n++));
+        }
+    }
+}
